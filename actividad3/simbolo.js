0cb1b0771db42ad696954b87d1ed393b8d0c224a
davidmonroy <david.m.cotamo@hotmail.com>
2015-11-09 08:33:47 -0500

    actividad 3 comprobacion de respuestas correctas e incorrectas

--- actividad3/simbolo.js
+++ actividad3/simbolo.js
@@ -1,10 +1,14 @@
 var respuestas = document.querySelectorAll(".clickable")
+,btnComprobar = document.getElementById("comprobar")
+,h_s_estado = document.getElementById("h_s_estado_js")
 ,resBien = 0
 ,resMal = 0
+
 for(i=0; i<respuestas.length; i++){
 	respuestas[i].addEventListener("click", validarRes)
 }
 
+btnComprobar.addEventListener("click", comprobarRes)
 
 function validarRes(){
 	var res = this.getAttribute("resp-data")
@@ -16,4 +20,36 @@ function validarRes(){
 		resMal += 1
 		console.log("mal")
 	}
+	var x = this.parentNode.parentNode
+	x.classList.add("dis")
+	
+	var el = x
+	,elClone = el.cloneNode(true)
+	
+	el.parentNode.replaceChild(elClone, el)
+}
+
+function comprobarRes(){
+	var minResCorrectas = 4
+	,article =document.createElement("article")
+	,estado =document.createElement("h2")
+	,respuestasBien =document.createElement("p")
+	,respuestasMal =document.createElement("p")
+	
+	if(resBien>minResCorrectas){
+		article.classList.add("pasaste")
+		estado.innerText = "Pasaste"
+		this.disabled = true
+	}else{
+		article.classList.add("perdiste");
+		estado.innerText = "Perdio"
+		this.disabled = true
+	}
+	respuestasBien.innerText = "Respuestas Correctas: " + resBien
+	respuestasMal.innerText = "Respuestas Incorrectas: " + resMal
+
+	article.appendChild(estado)
+	article.appendChild(respuestasBien)
+	article.appendChild(respuestasMal)
+	h_s_estado.appendChild(article)
 }
